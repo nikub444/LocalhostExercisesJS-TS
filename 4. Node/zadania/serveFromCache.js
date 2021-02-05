6ac7e8275bb0913812a5d258237f3f62329b80d5
Nowogrodzki <maciej.nowogrodzki.97@wp.pl>
2021-02-05 12:15:56 +0100

    deleted last two .js files

--- 4. Node/zadania/serveFromCache.js
+++
@@ -1,7 +0,0 @@
-// stwórz fukcjonalność do zaciągania i cachowania danych z https://www.googleapis.com/books/v1/volumes?q=clarcson		
-
-
-// zbudować funkcjonalność, która			
-// - zaciągnie dane poprzez axiosa/fetcha z open api google books na podstawie danego query			
-// - dla każdego query (q=) który wpisze user, fn zapiszę plik json w folderze /cache z nazwą która jest slugiem query oraz wysle do użytkownika dane o tym query			
-// - ponowne wywołanie danej funkcjonalności z tym samym query powoduje zaserwowanie danych z pliku json			
