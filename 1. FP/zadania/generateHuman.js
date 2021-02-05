498d9ba2686755d2c92b882e893f28e4d6a22729
Nowogrodzki <maciej.nowogrodzki.97@wp.pl>
2021-02-05 11:21:48 +0100

    .js files removed

--- 1. FP/zadania/generateHuman.js
+++
@@ -1,7 +0,0 @@
-// Stwórz funkcję generateHuman() która będzie tworzyć obiekt o podanej niżej strukturze ale o losowych wartościach
-// {name, surname, email:(w oparciu o name i surname), age:(przedział 18-85), phoneNr:random 9 numbers ,country:oneOf([PL,UK,USA])}
-// oraz _id = objectId() - wykorzystać bibliotekę  https://www.npmjs.com/package/uuid
-
-function generateHuman(){
-
-}
