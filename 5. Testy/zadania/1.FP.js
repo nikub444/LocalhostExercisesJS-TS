85494952b374245f6f49571042f8498940951da9
Maciej Nowogrodzki <maciej.nowogrodzki.97@wp.pl>
2021-02-10 12:12:50 +0100

    removing fp.js

--- 5. Testy/zadania/1.FP.js
+++
@@ -1,5 +0,0 @@
-// Napisz testy do modułu 1 testując każde zadanie wg warunków:
-
-// - jak się zachowuje wrzucamy do niej dobre/złe argumenty
-// - co z niej wychodzi jak wrzucamy dobre/złe argumenty
-// - czy poprawnie się zachowuje jak wrzucamy do niej złe argumenty
