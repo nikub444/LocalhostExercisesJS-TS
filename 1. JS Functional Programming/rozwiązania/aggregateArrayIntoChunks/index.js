91a32a162627e531e83beddddcc5aa04652ab23c
nikub444 <magaszak12@gmail.com>
2021-09-02 16:23:07 +0200

    arrayIntoChunks

--- 1. JS Functional Programming/rozwiązania/aggregateArrayIntoChunks/index.js
+++ 1. JS Functional Programming/rozwiązania/aggregateArrayIntoChunks/index.js
@@ -1,2 +1,34 @@
-var a = 100;
-console.log(a);
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
+var alphabet = "abcdefghijklmnoprstuwxyz".split("");
+function aggregateIntoChunks(array) {
+    var lenghtOfChunksToGenerate = chunksGenerator(array.length);
+    var result = [];
+    var copyOfArrayForUseInSplice = __spreadArray([], array);
+    lenghtOfChunksToGenerate.forEach(function (value) {
+        return result.push(copyOfArrayForUseInSplice.splice(0, value));
+    });
+    return result;
+}
+function chunksGenerator(totalLength) {
+    if (totalLength <= 3) {
+        throw Error("Given lenght must have lenght grater then 3");
+    }
+    var i = 0;
+    var arrayOfChunksLenght = [];
+    while (i < totalLength) {
+        var generatedNumber = Math.floor(Math.random() * (7 - 4) + 4);
+        i += generatedNumber;
+        if (i > totalLength) {
+            arrayOfChunksLenght.length = 0;
+            i = 0;
+        }
+        else {
+            arrayOfChunksLenght.push(generatedNumber);
+        }
+    }
+    return arrayOfChunksLenght;
+}
--- 1. JS Functional Programming/rozwiązania/aggregateArrayIntoChunks/index.ts
+++ 1. JS Functional Programming/rozwiązania/aggregateArrayIntoChunks/index.ts
@@ -1,2 +1,32 @@
-var a = 100;
-console.log(a)
+const alphabet = "abcdefghijklmnoprstuwxyz".split("");
+
+function aggregateIntoChunks(
+    array: Array<string>
+  ): Array<Array<string>> {
+    const lenghtOfChunksToGenerate = chunksGenerator(array.length);
+    const result: Array<Array<string>> = [];
+    const copyOfArrayForUseInSplice = [...array];
+    lenghtOfChunksToGenerate.forEach((value) =>
+      result.push(copyOfArrayForUseInSplice.splice(0, value))
+    );
+    return result;
+  }
+
+  function chunksGenerator(totalLength: number): Array<number> {
+    if (totalLength <= 3) {
+      throw Error("Given lenght must have lenght grater then 3");
+    }
+    let i = 0;
+    const arrayOfChunksLenght = [];
+    while (i < totalLength) {
+      let generatedNumber = Math.floor(Math.random() * (7 - 4) + 4);
+      i += generatedNumber;
+      if (i > totalLength) {
+        arrayOfChunksLenght.length = 0;
+        i = 0;
+      } else {
+        arrayOfChunksLenght.push(generatedNumber);
+      }
+    }
+    return arrayOfChunksLenght;
+  }
